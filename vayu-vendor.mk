b09693a9b052f6cf4c9b01c1dcd7934fee908e47
kubersharma001 <kubersharma001@gmail.com>
2021-04-23 09:47:58 +0000

    vayu: Move libvolumelistener.so to vendor_overlay

--- vayu-vendor.mk
+++ vayu-vendor.mk
@@ -158,8 +158,8 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/vayu/proprietary/system_ext/lib64/vendor.qti.hardware.sigma_miracast@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.hardware.sigma_miracast@1.0.so \
     vendor/xiaomi/vayu/proprietary/system_ext/lib64/vendor.qti.hardware.wifidisplaysession@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.hardware.wifidisplaysession@1.0.so \
     vendor/xiaomi/vayu/proprietary/system_ext/lib64/vendor.qti.imsrtpservice@3.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.imsrtpservice@3.0.so \
-    vendor/xiaomi/vayu/proprietary/system/lib/libvolumelistener.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libvolumelistener.so \
-    vendor/xiaomi/vayu/proprietary/system/lib64/libvolumelistener.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libvolumelistener.so
+    vendor/xiaomi/vayu/proprietary/product/vendor_overlay/30/lib/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/soundfx/libvolumelistener.so \
+    vendor/xiaomi/vayu/proprietary/product/vendor_overlay/30/lib64/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib64/soundfx/libvolumelistener.so
 
 PRODUCT_PACKAGES += \
     PowerOffAlarm \
